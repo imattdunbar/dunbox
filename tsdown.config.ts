71bcef0e4d395355c6a355e41c09c4fa81f731a4
Matt Dunbar <me@mattdunbar.io>
2026-08-27 18:06:40 -0400

    published

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'tsdown'
 
-export default defineConfig({
-  workspace: [
-    // packages
-    'packages/core',
-    'packages/ui'
-  ],
-  entry: 'src/index.ts',
-  dts: true,
-  exports: true
-})
+export default defineConfig([
+  {
+    name: 'core',
+    cwd: 'packages/core',
+    entry: 'src/index.ts',
+    dts: true,
+    exports: true
+  },
+  {
+    name: 'ui',
+    cwd: 'packages/ui',
+    entry: ['src/**/*.ts', 'src/**/*.tsx'],
+    platform: 'browser',
+    dts: true,
+    copy: 'src/styles.css',
+    exports: { customExports: { './styles.css': './dist/styles.css' } }
+  }
+])
